4d18504bb41c405ebc97a89ede141447bbd2463b
Brian Hainline <bhainline926@gmail.com>
2018-09-03 20:20:43 -0700

    Assignment Buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,14 @@ class Album extends Component {
       this.setSong(newSong);
       this.play();
     }
+
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex === 4 ? currentIndex : currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
     
 
  
@@ -103,9 +111,8 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
            />
-          }
-          }
        </section>
      );
    }
